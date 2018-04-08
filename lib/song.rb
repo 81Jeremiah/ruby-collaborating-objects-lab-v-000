807da7fb634be0a4449f6659d8b76fba9f619c59
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-04-08 20:18:26 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,8 @@ class Song
 
   def self.new_by_filename(file_name)
     file_split = file_name.split(" - ")
-    name = self.new(file_split[1])
+    song = self.new(file_split[1])
     artist = Artist.find_or_create_by_name(file_split[0])
+    
   end
 end
