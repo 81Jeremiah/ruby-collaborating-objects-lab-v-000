e2e62fd0eb16248a5ebba70b70ea31f161d18aa2
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-04-08 19:55:28 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -13,8 +13,8 @@ class MP3Importer
   end
 
   def import
-    imports = 
-    self.files.grep(/*\.mp3$/)
+    imports = self.files
+    imports.grep(/*\.mp3$/)
     #self.files.each{ |file| Artist.find_or_create_by_name(file) }
     #binding.pry
     #Artist.save
